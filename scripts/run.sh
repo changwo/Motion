556877e9ba71fa92de00c394d5abebec2de4f987
Habib Kadiri <kadirit@hotmail.com>
2020-06-24 16:52:18 +0200

    added the run script command

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 python manage.py collectstatic --no-input
-gunicorn --reload -w 4 -b 0.0.0.0:8000 project.wsgi:application
+gunicorn -w 4 -b 0.0.0.0:8000 project.wsgi:application
