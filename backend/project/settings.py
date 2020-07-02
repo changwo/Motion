7b6ebdf0f0c8942d7ab1f75d0980a75648184612
Habib Kadiri <kadirit@hotmail.com>
2020-07-02 15:34:25 +0200

    fixed conf file

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static-files/'
 STATIC_ROOT = '/static/'
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = '/media/'
