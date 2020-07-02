c540da4454b40a0406eef5057051267568f941fd
Habib Kadiri <kadirit@hotmail.com>
2020-07-02 17:58:41 +0200

    fixed conf file

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -61,15 +61,25 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-MIDDLEWARE_CLASSES = (
+    # CORS headers stuff
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-)
-CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
-CORS_ALLOW_CREDENTIALS = True# If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
+]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000/',
+]
 CORS_ORIGIN_REGEX_WHITELIST = [
-    'http://localhost:3030',
+    'http://localhost:3000/',
+]
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
