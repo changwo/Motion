3f9bc937ec84a8b4c648ff540c165b7a2b88239a
Habib Kadiri <kadirit@hotmail.com>
2020-07-02 17:39:04 +0200

    really added cors?

--- frontend/src/store/types.js
+++ frontend/src/store/types.js
@@ -32,5 +32,5 @@ export const SET_SINGLE_PROFILE_POST = "SET_SINGLE_PROFILE_POST";
 export const SET_ERROR = "SET_ERROR";
 export const CLEAR_ERROR = "CLEAR_ERROR";
 
-export const baseUrl = "https://habib-kadiri.propulsion-learn.ch/backend/api/";
-// export const baseUrl = "http://localhost:8000/backend/api/";
+// export const baseUrl = "https://habib-kadiri.propulsion-learn.ch/backend/api/";
+export const baseUrl = "http://localhost:8000/backend/api/";
